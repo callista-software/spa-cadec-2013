e06a6889705b9daac7e911ded184a95b5e41dbb6
Johannes Carlén <johannes.carlen@callistaenterprise.se>
2012-12-18 08:35:42 +0100

    Updated api.js with read/save methods on products

--- api.js
+++ api.js
@@ -11,31 +11,31 @@ SpaApi.prototype.start = function() {
     var that = this;
 
     this.products = [{
-        'id' : 1,
+        'id' : 0,
         'name' : 'Limefrukt',
         'description' : 'Underbara limefrukter från sydamerika',
         'inStock' : 20,
         'imgSrc' : 'lime.png'
     },{
-        'id' : 2,
+        'id' : 1,
         'name' : 'Äpplen',
         'description' : 'Fina svenska äpplen',
         'inStock' : 70,
         'imgSrc' : 'apple.png'
     }, {
-        'id' : 3,
+        'id' : 2,
         'name' : 'Bananer',
         'description' : 'Bananer från Brasilien',
         'inStock' : 50,
         'imgSrc' : 'banana.png'
     }, {
-        'id' : 4,
+        'id' : 3,
         'name' : 'Mango',
-        'description' : 'Fina fräscha Mangos',
+        'description' : 'Fina fräsha Mangos',
         'inStock' : 1,
         'imgSrc' : 'mango.png'
     }, {
-        'id' : 5,
+        'id' : 4,
         'name' : 'Kiwi',
         'description' : 'Färska Kiwifrukter',
         'inStock' : 10,
@@ -44,6 +44,29 @@ SpaApi.prototype.start = function() {
 
     this.app.get('/products', function(req, res) {
         that.response(res, JSON.stringify(that.products));
+    });
+    
+    this.app.get('/products/:id', function(req, res) {
+        console.log('Get product with id %s', req.params.id);
+        that.response(res, JSON.stringify(that.products[req.params.id]));
+    });
+
+    this.app.put('/products', function(req, res) {
+        console.log('PUT new data: %s', req.body.name);
+        var fruit = req.body;
+        fruit.id = that.products.length;
+        that.products.push(fruit);
+        that.response(res, JSON.stringify(fruit));
+    });
+
+    this.app.put('/products/:id', function(req, res) {
+        console.log('PUT data: [%s]:%s', req.params.id, req.body.name);
+        var fruit = that.products[req.params.id];
+        fruit.name = req.body.name;
+        fruit.description = req.body.description;
+        fruit.inStock = req.body.inStock;
+        fruit.imgSrc = req.body.imgSrc;
+        that.response(res, JSON.stringify(fruit));
     });
     
     this.app.get('/carts/:id', function(req, res) {
